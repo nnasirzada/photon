8ddf95527e247596cb4aeb143eae6b526dde58d4
shahinmursalov <smursalov2018@ada.edu.az>
2018-02-23 16:42:01 +0400

    Database connection made

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../database/');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  // db.getConnection((err, con) => {
+  //   if (err) throw err;
+  //   con.query('SELECT * FROM person', (err, results, fields) => {
+  //     con.release();
+  //     console.log(results);
+  //   });
+  // });
   res.render('index', { title: 'Express', layout: false });
 });
 
