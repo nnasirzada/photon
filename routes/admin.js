c4eaf9a54be13e4d6cf1206e51b6c2e9a3ed3fa4
Nazar Nasirzada (home) <nezer.n@code.edu.az>
2018-03-15 13:56:58 +0400

    Added admin page layout and index

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,16 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+router.all('/*', (req, res, next) => {
+	if (!req.isAuthenticated() || req.user.type != 'admin') {
+		res.redirect('/auth/login/');
+	}
+	req.app.locals.layout = 'admin/layout';
+	next();
+});
+
 router.get('/', (req, res, next) => {
-	if (!req.isAuthenticated() || req.user.type != 'admin')
-		return res.redirect('/auth/login/');
-	res.render('admin/index', { layout: false });
+	res.render('admin/index');
 });
 
 module.exports = router;
