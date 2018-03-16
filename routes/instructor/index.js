8d7c0376b4912f8367b5524ca8d454a0ec314106
Nazar <nezer.n@code.edu.az>
2018-03-16 17:47:55 +0400

    Instructor routes, views added

--- routes/instructor/index.js
+++ routes/instructor/index.js
@@ -2,4 +2,18 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+router.all('/*', (req, res, next) => {
+	if (!req.isAuthenticated() || req.user.type != 'instructor') {
+		res.redirect('/auth/login/');
+	}
+	req.app.locals.layout = 'instructor/layout';
+	next();
+});
+
+router.get('/', (req, res, next) => {
+	res.render('instructor/index', { title: 'Home - Instructor Dashboard', email: req.user.email });
+});
+
+router.use('/classes', require('./classes'));
+
 module.exports = router;
