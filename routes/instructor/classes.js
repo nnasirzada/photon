6ebbbe7abb950708c4029664f7ff1f1dd36e529b
Nazar <nezer.n@code.edu.az>
2018-03-19 00:41:15 +0400

    class homepage created and class route modified

--- routes/instructor/classes.js
+++ routes/instructor/classes.js
@@ -19,7 +19,7 @@ router.get('/', (req, res, next) => {
 	}).catch(console.error);
 });
 
-router.get('/:termId', (req, res, next) => {
+router.get('/term/:termId', (req, res, next) => {
 	models.Instructor.getTeachingClasses(req.user.id, req.params.termId).then(classes => {
 		res.render("instructor/classes/by-term", {
 			title: "Classes - Instructor Dashboard",
@@ -36,20 +36,30 @@ router.get('/:termId', (req, res, next) => {
 	}).catch(console.error);
 });
 
-// router.get('/:termId/:classId', (req, res, next) => {
-// 	models.Instructor.getTeachingClasses(req.user.id, req.params.termId).then(classes => {
-// 		res.render("instructor/classes/homepage", {
-// 			title: "Classes - Instructor Dashboard",
-// 			active: {
-// 				classes: true
-// 			},
-// 			imports: {
-// 				uikit: true
-// 			},
-// 			classFound: classes.length > 0,
-// 			classes: classes
-// 		});
-// 	}).catch(console.error);
-// })
+router.get('/term/:termId/class/:classId', (req, res, next) => {
+	Promise.all([
+		models.sequelize.query("SELECT a.id AS `class_id`, a.section, a.max_enrollment, (SELECT COUNT(id) FROM class_enrollment WHERE class_id = ?) AS enrollment_count, c.number AS `course_number`, c.name AS `course_name`, c.description AS `course_description`, g.code AS `grade_mode_code`, g.name AS `grade_mode_name`, st.code AS `schedule_type_code`, st.name AS `schedule_type_name`, s.code AS `subject_code`, s.name AS `subject_name`, sc.name AS `school_name`, sc.code AS `school_code`, CONCAT(t.name, ' - ', pot.name) AS `term_name`, pot.start_date, pot.end_date FROM (SELECT * FROM class WHERE id = ? AND instructor_id = ?) a LEFT JOIN course c ON a.course_id = c.id LEFT JOIN grade_mode g ON c.grade_mode_id = g.id LEFT JOIN schedule_type st ON c.schedule_type_id = st.id LEFT JOIN subject s ON c.subject_id = s.id LEFT JOIN school sc ON c.school_id = sc.id LEFT JOIN part_of_term pot ON a.part_of_term_id = pot.id LEFT JOIN term t ON pot.term_id = t.id", {
+			replacements: [req.params.classId, req.params.classId, req.user.id],
+			type: models.sequelize.QueryTypes.SELECT
+		}),
+		models.sequelize.query('SELECT cm.monday, cm.tuesday, cm.wednesday, cm.thursday, cm.friday, cm.saturday, cm.sunday, cm.start_time, cm.end_time, r.code AS `room_code`, r.name AS `room_name`, b.code AS `building_code`, b.name AS `building_name` FROM (SELECT cm.* FROM class_meeting cm LEFT JOIN class c ON cm.class_id = c.id WHERE c.id = ? AND c.instructor_id = ?) cm LEFT JOIN room r ON cm.room_id = r.id LEFT JOIN building b ON r.building_id = b.id', {
+			replacements: [req.params.classId, req.user.id],
+			type: models.sequelize.QueryTypes.SELECT
+		})
+	]).then(values => {
+		console.log(values);
+		res.render("instructor/classes/homepage", {
+			title: "Classes - Instructor Dashboard",
+			active: {
+				classes: true
+			},
+			imports: {
+				uikit: true
+			},
+			classDetails: values[0][0],
+			classMeetings: values[1][0]
+		});
+	}).catch(console.error);
+})
 
 module.exports = router;
