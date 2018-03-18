4c0800b015239a586c4cd9e9246c029505bfd34d
Nazar <nezer.n@code.edu.az>
2018-03-18 17:54:15 +0400

    Instructor/Classes Modified

--- routes/instructor/classes.js
+++ routes/instructor/classes.js
@@ -1,7 +1,55 @@
 const express = require('express');
 const passport = require('passport');
+const models = require('../../models');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+	models.Instructor.getTeachingTerms(req.user.id).then(terms => {
+		res.render("instructor/classes", {
+			title: "Select a term - Instructor Dashboard",
+			active: {
+				classes: true
+			},
+			imports: {
+				uikit: true
+			},
+			resultFound: terms.length > 0,
+			result: terms
+		});
+	}).catch(console.error);
+});
 
+router.get('/:termId', (req, res, next) => {
+	models.Instructor.getTeachingClasses(req.user.id, req.params.termId).then(classes => {
+		res.render("instructor/classes/by-term", {
+			title: "Classes - Instructor Dashboard",
+			active: {
+				classes: true
+			},
+			imports: {
+				uikit: true
+			},
+			resultFound: classes.length > 0,
+			result: classes,
+			termId: req.params.termId
+		});
+	}).catch(console.error);
+});
+
+// router.get('/:termId/:classId', (req, res, next) => {
+// 	models.Instructor.getTeachingClasses(req.user.id, req.params.termId).then(classes => {
+// 		res.render("instructor/classes/homepage", {
+// 			title: "Classes - Instructor Dashboard",
+// 			active: {
+// 				classes: true
+// 			},
+// 			imports: {
+// 				uikit: true
+// 			},
+// 			classFound: classes.length > 0,
+// 			classes: classes
+// 		});
+// 	}).catch(console.error);
+// })
 
 module.exports = router;
