7433975a577a009128273d2ac5448e3c0e921dd0
Nazar <nezer.n@code.edu.az>
2018-03-21 17:19:12 +0400

    students-list and grade-components routes exported

--- routes/instructor/classes.js
+++ routes/instructor/classes.js
@@ -18,6 +18,7 @@ router.get('/', (req, res, next) => {
 	}).catch(console.error);
 });
 
+// Select a term page
 router.get('/term/:termId', (req, res, next) => {
 	models.Instructor.getTeachingClasses(req.user.id, req.params.termId).then(classes => {
 		res.render('instructor/classes/by-term', {
@@ -35,6 +36,7 @@ router.get('/term/:termId', (req, res, next) => {
 	}).catch(console.error);
 });
 
+// Select a class page
 router.get('/class/:classId', (req, res, next) => {
 	Promise.all([
 		models.sequelize.query("SELECT a.id AS `class_id`, a.section, c.number AS `course_number`, c.name AS `course_name`, c.description AS `course_description`, g.name AS `grade_mode_name`, st.name AS `schedule_type_name`, s.code AS `subject_code`, s.name AS `subject_name`, sc.name AS `school_name`, CONCAT(t.name, ' - ', pot.name) AS `term_name`, DATE_FORMAT(pot.start_date, '%c/%d/%Y') AS `start_date`, DATE_FORMAT(pot.end_date, '%c/%d/%Y') AS `end_date` FROM (SELECT * FROM class WHERE id = ? AND instructor_id = ?) a LEFT JOIN course c ON a.course_id = c.id LEFT JOIN grade_mode g ON c.grade_mode_id = g.id LEFT JOIN schedule_type st ON c.schedule_type_id = st.id LEFT JOIN subject s ON c.subject_id = s.id LEFT JOIN school sc ON c.school_id = sc.id LEFT JOIN part_of_term pot ON a.part_of_term_id = pot.id LEFT JOIN term t ON pot.term_id = t.id", {
@@ -63,31 +65,10 @@ router.get('/class/:classId', (req, res, next) => {
 	}).catch(console.error);
 });
 
-router.get('/class/:classId/students-list', (req, res, next) => {
-	res.render('instructor/classes/students-list', {
-		title: 'List of Enrolled Students',
-		active: {
-			classes: true
-		},
-		imports: {
-			jquery: true,
-			jquery_ui: true,
-			uikit: true,
-			data_tables: true
-		}
-	});
-});
+// Students List
+router.use('/class/:classId/students-list/', require('./students-list'));
 
-router.get('/class/:classId/students-list/all', (req, res, next) => {
-	models.sequelize.query("SELECT s.user_id AS `student_id`, u.firstname, u.lastname, u.patronymic, u.email, m.code AS `major`, ce.status FROM (SELECT ce.student_id, ce.status FROM class_enrollment ce LEFT JOIN class c ON ce.class_id = c.id WHERE c.instructor_id = ? AND ce.class_id = ? AND ce.status != 'dropped') ce LEFT JOIN student s ON ce.student_id = s.user_id LEFT JOIN user u ON s.user_id = u.id LEFT JOIN major m ON s.major_id = m.id", {
-		replacements: [req.user.id, req.params.classId],
-		type: models.sequelize.QueryTypes.SELECT
-	}).then(values => {
-		let json = {};
-		json['data'] = values;
-		res.status(200).json(json);
-	}).catch(console.error);
-});
-
+// Grade Components
+router.use('/class/:classId/grade-components/', require('./grade-components'));
 
 module.exports = router;
