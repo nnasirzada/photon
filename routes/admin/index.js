fea622fcb093ed77088ad0a23ec65ff0b5730feb
shahinmursalov <smursalov2018@ada.edu.az>
2018-03-23 02:42:10 +0400

    Just Because

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -13,6 +13,7 @@ router.get("/", (req, res, next) => {
   res.redirect('/admin/buildings/');
 });
 
+router.use("/users/admins/", require("./admins.js"));
 router.use("/buildings/", require("./buildings.js"));
 router.use("/classes/", require("./classes.js"));
 router.use("*/courses/:course_id/classes/", require("./classes.js"));
@@ -24,7 +25,7 @@ router.use("/programs/:program_id/courses/", require("./courses.js"));
 router.use("/courses/", require("./courses.js"));
 router.use("/grade-modes/", require("./grade-modes.js"));
 router.use("/grade-modes/:grade_mode_id/scale/", require("./grade-scale.js"));
-router.use("/instructors/", require("./instructors.js"));
+router.use("/users/instructors/", require("./instructors.js"));
 router.use("/majors/", require("./majors.js"));
 router.use("*/classes/:class_id/meetings/", require("./meetings.js"));
 router.use("*/courses/:course_id/prerequisites/", require("./prerequisites.js"));
