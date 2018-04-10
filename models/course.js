c881013566e6129532c444063f356636f4aa21d2
Javid <jmammadov2018@ada.edu.az>
2018-04-10 14:10:44 +0400

    View Grades Done

--- models/course.js
+++ models/course.js
@@ -1,49 +1,56 @@
 module.exports = (sequelize, DataTypes) => {
 
-	const Course = sequelize.define('Course', {
-		id: {
-			type: DataTypes.MEDIUMINT(8).UNSIGNED,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		number: {
-			type: DataTypes.MEDIUMINT(8).UNSIGNED,
-		},
-		name: {
-			type: DataTypes.STRING(191)
-		},
-		description: {
-			type: DataTypes.TEXT()
-		},
-		credit_hours: {
-			type: DataTypes.DECIMAL(4, 2)
-		},
-		gpa_hours: {
-			type: DataTypes.DECIMAL(4, 2)
-		},
-		passing_grade_id: {
-			type: DataTypes.SMALLINT(5).UNSIGNED
-		},
-		schedule_type_id: {
-			type: DataTypes.SMALLINT(5).UNSIGNED
-		},
-		grade_mode_id: {
-			type: DataTypes.SMALLINT(5).UNSIGNED
-		},
-		program_id: {
-			type: DataTypes.SMALLINT(5).UNSIGNED
-		},
-		subject_id: {
-			type: DataTypes.SMALLINT(5).UNSIGNED
-		},
-		school_id: {
-			type: DataTypes.SMALLINT(5).UNSIGNED
-		},
-		deleted: {
-			type: DataTypes.BOOLEAN(),
-			defaultValue: false
-		}
-	}, { tableName: 'course' });
+    const Course = sequelize.define('Course', {
+        id: {
+            type: DataTypes.MEDIUMINT(8).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        number: {
+            type: DataTypes.MEDIUMINT(8).UNSIGNED,
+        },
+        name: {
+            type: DataTypes.STRING(191)
+        },
+        description: {
+            type: DataTypes.TEXT()
+        },
+        credit_hours: {
+            type: DataTypes.DECIMAL(4, 2)
+        },
+        gpa_hours: {
+            type: DataTypes.DECIMAL(4, 2)
+        },
+        passing_grade_id: {
+            type: DataTypes.SMALLINT(5).UNSIGNED
+        },
+        schedule_type_id: {
+            type: DataTypes.SMALLINT(5).UNSIGNED
+        },
+        grade_mode_id: {
+            type: DataTypes.SMALLINT(5).UNSIGNED
+        },
+        program_id: {
+            type: DataTypes.SMALLINT(5).UNSIGNED
+        },
+        subject_id: {
+            type: DataTypes.SMALLINT(5).UNSIGNED
+        },
+        school_id: {
+            type: DataTypes.SMALLINT(5).UNSIGNED
+        },
+        deleted: {
+            type: DataTypes.BOOLEAN(),
+            defaultValue: false
+        }
+    }, {tableName: 'course'});
 
-	return Course;
+    Course.getCourseDetailsByClassId = class_id => {
+        return sequelize.query('select su.code as code, co.number as number, co.name as name, cl.section as section from class cl join course co on cl.deleted = false and cl.id = ? and co.deleted = false and cl.course_id = co.id join subject su on su.deleted = false and co.subject_id = su.id', {
+            replacements: [class_id],
+            type: sequelize.QueryTypes.SELECT
+        });
+    };
+
+    return Course;
 };
--- routes/student/schedule.js
+++ routes/student/schedule.js
@@ -5,82 +5,82 @@ const moment = require('moment');
 const RRule = require('rrule').RRule;
 
 router.get('/', (req, res, next) => {
-	res.render('student/schedule', {
-		title: 'Schedule',
-		active: {
-			schedule: true
-		},
-		imports: {
-			jquery: true,
-			uikit: true,
-			moment: true,
-			fullcalendar: true
-		}
-	});
+    res.render('student/schedule', {
+        title: 'Schedule',
+        active: {
+            schedule: true
+        },
+        imports: {
+            jquery: true,
+            uikit: true,
+            moment: true,
+            fullcalendar: true
+        }
+    });
 });
 
 router.get('/events', (req, res, next) => {
-	if (req.query.start && req.query.end && moment(req.query.start, 'YYYY-M-DD', true).isValid() && moment(req.query.end, 'YYYY-M-DD', true).isValid()) {
-		models.sequelize.query("SELECT cm.*, c.name AS `course_name` FROM (SELECT id, course_id FROM class WHERE deleted = 0 AND id in (SELECT class_id FROM `class_enrollment` WHERE deleted = 0 AND student_id = ?) AND part_of_term_id IN (SELECT id FROM part_of_term WHERE deleted = 0 AND term_id = (SELECT id FROM term WHERE (? BETWEEN start_date AND end_date) AND deleted = 0 ORDER BY start_date DESC LIMIT 1))) a INNER JOIN class_meeting cm ON a.id = cm.class_id LEFT JOIN course c ON a.course_id = c.id WHERE cm.deleted = 0", {
-			replacements: [req.user.id, req.query.start],
-			type: models.sequelize.QueryTypes.SELECT
-		}).then(values => {
-			let colors = [
-				{ code: '#F5D76A' },
-				{ code: '#6699CC' },
-				{ code: '#E78B6C' },
-				{ code: '#5FB3B3' },
-				{ code: '#C594C5' },
-				{ code: '#82B440' },
-				{ code: '#279A54' }
-			];
-			let usedColors = {};
-			let events = [];
+    if (req.query.start && req.query.end && moment(req.query.start, 'YYYY-M-DD', true).isValid() && moment(req.query.end, 'YYYY-M-DD', true).isValid()) {
+        models.sequelize.query("SELECT distinct te.id as term_id, cm.*, c.name AS `course_name` FROM (SELECT id, course_id, part_of_term_id FROM class WHERE deleted = 0 AND id in (SELECT class_id FROM `class_enrollment` WHERE deleted = 0 AND student_id = ?) AND part_of_term_id IN (SELECT id FROM part_of_term WHERE deleted = 0 AND term_id = (SELECT id FROM term WHERE (? BETWEEN start_date AND end_date) AND deleted = 0 ORDER BY start_date DESC LIMIT 1))) a INNER JOIN class_meeting cm ON a.id = cm.class_id LEFT JOIN course c ON c.deleted = 0 and a.course_id = c.id join part_of_term pt on pt.deleted = 0 and a.part_of_term_id = pt.id join term te on te.deleted = 0 and pt.term_id = te.id WHERE cm.deleted = 0", {
+            replacements: [req.user.id, req.query.start],
+            type: models.sequelize.QueryTypes.SELECT
+        }).then(values => {
+            let colors = [
+                {code: '#F5D76A'},
+                {code: '#6699CC'},
+                {code: '#E78B6C'},
+                {code: '#5FB3B3'},
+                {code: '#C594C5'},
+                {code: '#82B440'},
+                {code: '#279A54'}
+            ];
+            let usedColors = {};
+            let events = [];
 
-			for (let i = 0; i < values.length; i++) {
-				let byweekday = [];
-				let eventColor;
+            for (let i = 0; i < values.length; i++) {
+                let byweekday = [];
+                let eventColor;
 
-				if (usedColors[values[i].class_id]) {
-					eventColor = usedColors[values[i].class_id];
-				}
-				else {
-					eventColor = colors.shift().code;
-					usedColors[values[i].class_id] = eventColor;
-				}
+                if (usedColors[values[i].class_id]) {
+                    eventColor = usedColors[values[i].class_id];
+                }
+                else {
+                    eventColor = colors.shift().code;
+                    usedColors[values[i].class_id] = eventColor;
+                }
 
-				if (values[i].monday) byweekday.push(RRule.MO);
-				if (values[i].tuesday) byweekday.push(RRule.TU);
-				if (values[i].wednesday) byweekday.push(RRule.WE);
-				if (values[i].thursday) byweekday.push(RRule.TH);
-				if (values[i].friday) byweekday.push(RRule.FR);
-				if (values[i].saturday) byweekday.push(RRule.SA);
-				if (values[i].sunday) byweekday.push(RRule.SU);
+                if (values[i].monday) byweekday.push(RRule.MO);
+                if (values[i].tuesday) byweekday.push(RRule.TU);
+                if (values[i].wednesday) byweekday.push(RRule.WE);
+                if (values[i].thursday) byweekday.push(RRule.TH);
+                if (values[i].friday) byweekday.push(RRule.FR);
+                if (values[i].saturday) byweekday.push(RRule.SA);
+                if (values[i].sunday) byweekday.push(RRule.SU);
 
-				let rule = new RRule({
-					freq: RRule.WEEKLY,
-					byweekday: byweekday,
-					dtstart: new Date(req.query.start + ' ' + values[i].start_time),
-					until: new Date(req.query.end)
-				});
+                let rule = new RRule({
+                    freq: RRule.WEEKLY,
+                    byweekday: byweekday,
+                    dtstart: new Date(req.query.start + ' ' + values[i].start_time),
+                    until: new Date(req.query.end)
+                });
 
-				rule.all((date, index) => {
-					return events.push({
-						id: values[i].class_id,
-						title: values[i].course_name,
-						start: date,
-						end: moment(date).add('75', 'minutes').toDate(),
-						overlap: true,
-						url: '/student/classes/class/' + values[i].class_id,
-						color: eventColor
-					});
-				});
-			}
-			res.status(200).json(events);
-		}).catch(console.error);
-	} else {
-		throw new Error('Missing query params: start & end');
-	}
-})
+                rule.all((date, index) => {
+                    return events.push({
+                        id: values[i].class_id,
+                        title: values[i].course_name,
+                        start: date,
+                        end: moment(date).add('75', 'minutes').toDate(),
+                        overlap: true,
+                        url: '/student/grades/term/' + values[i].term_id + '/class/' + values[i].class_id,
+                        color: eventColor
+                    });
+                });
+            }
+            res.status(200).json(events);
+        }).catch(console.error);
+    } else {
+        throw new Error('Missing query params: start & end');
+    }
+});
 
 module.exports = router;
